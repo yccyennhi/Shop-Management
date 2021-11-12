639b7b525bfb456cdbcc90380d2f17e76c65be82
Đoàn Ngọc Lãm <19521737@gm.uit.edu.vn>
2021-11-12 21:51:15 +0700

    update_model

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,40 +1,9 @@
 import { PostModel } from '../models/PostModel.js'
 
 export const getPosts = async(req, res) => {
-    //res.send('ROUTER SUCCESS');
-    try {
-        const posts = await PostModel.find();
-        console.log('posts', posts);
-        res.status(200).json(posts);
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
+    res.send('ROUTER SUCCESS');
 };
 
 export const createPost = async (req, res) => {
-    //res.send('ROUTER SUCCESS');
-    try {
-        const newPost = req.body;
-
-        const post = new PostModel(newPost);
-
-        await post.save();
-
-        res.status(200).json(post);
-    } catch (e) {
-        res.status(500).json({ error: e });
-    }
-};
-
-export const updatePost = async (req, res) => {
-    //res.send('ROUTER SUCCESS');
-    try {
-        const updatePost = req.body;
-
-        const post = await PostModel.findOneAndUpdate({ _id: updatePost._id }, updatePost, { new: true });
-
-        res.status(200).json(post);
-    } catch (e) {
-        res.status(500).json({ error: e });
-    }
+    res.send('ROUTER SUCCESS');
 };
--- server/routers/PhieuDoiTras.js
+++ server/routers/PhieuDoiTras.js
@@ -1,1 +1,15 @@
-// JavaScript source code
+import express from "express";
+import {
+    getPhieuDoiTra,
+    createPhieuDouTra,
+    updatePhieuDouTra,
+} from "../controllers/PhieuDouTra.js";
+
+const router = express.Router();
+//http://localhost:5001/PhieuDouTras
+
+router.get("/", getPhieuDoiTra);
+router.post("/", createPhieuDouTra);
+router.post("/update", updatePhieuDouTra);
+
+export default router;

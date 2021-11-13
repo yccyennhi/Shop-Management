7bb92c9b61c7468668bb0f9407e8a9b7bd8d07bc
yccyennhi <ycc.yennhi@gmail.com>
2021-11-13 20:23:55 +0700

    13/11/2021
    Page Hàng Hóa

--- client/src/components/header/Headerbar/Headerbar.js
+++ client/src/components/header/Headerbar/Headerbar.js
@@ -2,31 +2,29 @@ import React from "react";
 import Logo from "../../../assets/Logo.png";
 import { Menu, Col, Row } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import "./styles.css";
 const { SubMenu } = Menu;
 
 function Headerbar() {
-  
   return (
     <div>
       <Row>
-            <Col span={6}>
-              <div className="logo">
-                <img src={Logo} alt="Website Logo"></img>
-              </div>
-            </Col>
-            <Col span={6} offset={12}>
-              <Menu
-                theme="dark"
-                mode="horizontal"
-                defaultSelectedKeys={["1000"]}
-              >
-                <SubMenu key="sub1" title="0585502434" icon={<UserOutlined />}>
-                  <Menu.Item key="subitem1">Tài khoản</Menu.Item>
-                  <Menu.Item key="subitem2">Đăng xuất</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Col>
-          </Row>
+        <Col span={6}>
+          <div className="logo">
+            <img src={Logo} alt="Website Logo"></img>
+          </div>
+        </Col>
+        <Col span={6} offset={12}>
+          <div className="account">
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1000"]}>
+              <SubMenu key="sub1" title="0585502434" icon={<UserOutlined />}>
+                <Menu.Item key="subitem1">Tài khoản</Menu.Item>
+                <Menu.Item key="subitem2">Đăng xuất</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }

59cab5214f4370af92e5cd0661a3a5f640df049d
Thao <19520280@gm.uit.edu.vn>
2021-11-25 08:11:43 +0700

    Bao cao draft

--- client/src/pages/BaoCaoPage/BCCuoiNgayPage.js
+++ client/src/pages/BaoCaoPage/BCCuoiNgayPage.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as actions from "../../redux/actions";
 import moment from "moment";
 import { PageHeader, Space, Card, DatePicker, Layout } from "antd";
 import CuoiNgaytable from "../../components/table/BaoCaoTable/CuoiNgaytable";
 
+import { CuoiNgaysState$ } from "../../redux/selectors";
 import COLOR from "../../color.js";
 
 const { Content, Sider } = Layout;
+
 export default function BCCuoiNgayPage() {
+  
+  const [currentDate, setCurrentDate] = useState(moment().startOf("day"));
+
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(actions.getCuoiNgays.getCuoiNgaysRequest());
+  }, [dispatch]);
+
+  const HoaDons = useSelector(CuoiNgaysState$);
+  const [dataSoure, setdataSoure] = useState(HoaDons);
+
+  React.useEffect(() => {
+    if (HoaDons.length) {
+      const data = HoaDons.filter(
+        (HoaDon) => HoaDon.ThoiGian.startOf("day") === currentDate.startOf("day")
+      );
+      setdataSoure(data);
+    }
+  }, [HoaDons, currentDate]);
+
+  console.log(currentDate, dataSoure);
   return (
     <Layout>
       <Layout>
@@ -27,7 +52,7 @@ export default function BCCuoiNgayPage() {
                 bordered={false}
                 style={{ width: 250, color: COLOR.darkblue }}
               >
-                <DatePicker defaultValue={moment()} format="DD/MM/YYYY" />
+                <DatePicker defaultValue={moment()} format="DD/MM/YYYY"  onChange={(e) => setCurrentDate(e)}/>
               </Card>
             </Space>
           </div>

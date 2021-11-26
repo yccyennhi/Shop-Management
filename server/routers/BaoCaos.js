6d36541c37d1abde9d43cf042bee587f62bcc4a8
Thao <19520280@gm.uit.edu.vn>
2021-11-26 22:26:38 +0700

    GiaNhap done

--- server/routers/BaoCaos.js
+++ server/routers/BaoCaos.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getCuoiNgays, getBCBanHangs } from './../controllers/BaoCao.js';
+import { getCuoiNgays, getBCBanHangs,getBCHangHoas } from './../controllers/BaoCao.js';
 
 const router = express.Router();
 //http://localhost:5000/HoaDons
 
 router.get("/CuoiNgays",getCuoiNgays );
 router.get("/BCBanHangs", getBCBanHangs);
-//router.get("/BCHangHoas", );
+router.get("/BCHangHoas", getBCHangHoas);
 export default router;

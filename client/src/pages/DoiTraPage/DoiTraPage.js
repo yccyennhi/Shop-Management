aea9e2217c043287e87cf7ec044e330cce821c46
yccyennhi <ycc.yennhi@gmail.com>
2021-12-13 12:13:50 +0700

    fix UI background sider

--- client/src/pages/DoiTraPage/DoiTraPage.js
+++ client/src/pages/DoiTraPage/DoiTraPage.js
@@ -154,7 +154,7 @@ export default function DoiTraPage() {
       <Layout>
         <Sider
           width={300}
-          style={{ padding: "0px 0px 0px 20px" }}
+          style={{ padding: "0px 0px 0px 20px", background:"#F0F2F5" }}
           className="site-layout-sider"
         >
           <div className="site-card-border-less-wrapper">

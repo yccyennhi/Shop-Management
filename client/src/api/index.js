86e2a9502c28192ffcf3b6b786a8401ad05a6ff1
yccyennhi <ycc.yennhi@gmail.com>
2021-12-15 00:27:50 +0700

    update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:5000";
+const URL = "https://nlmt-shop-management.herokuapp.com";
 
 //#region Auth
 export const loginTaiKhoan = (userForm) =>
--- client/src/constant.js
+++ client/src/constant.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = "https://nlmt-shop-management.herokuapp.com";
 export const FRONTEND_URL = "http://localhost:3000";
 
 export const INIT_STATE = {
